60fd8c0797643e9a5b7fca36af7f3aa538b3a734
jiuyuehe <xm.xiaom@gmail.com>
2014-07-24 10:55:48 +0800

    temlates user

--- app/views.py
+++ app/views.py
@@ -1,9 +1,27 @@
-
 from app import app
+from flask import render_template
+
 
 @app.route("/")
 @app.route("/index")
 def index():
-    user = {'nickname':'Miguel'}
-    return '''
-    '''
+    user = {'nickname': 'manbuzhiwu'}
+    return render_template('index.html',user = user)
+
+@app.route('/index1')
+def index1():
+    user = {'nickname':'manbu'}
+    post = [
+        {
+            'author':{'nickname':'Johnson'},
+            'body':'The Avengers movie was so cool!'
+        },
+        {
+            'author': { 'nickname': 'Susan' },
+            'body': 'The bar  was so white!'
+        }
+    ]
+    return render_template('index1.html',
+                           title ='Home',
+                           user = user,
+                           posts = post)
